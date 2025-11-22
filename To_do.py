3dcf2dec8c0ea2bb0308fdfcab3ab2f7b49e334b
Anupam Chaudhary <chaudharyanupam746@gmail.com>
2025-11-22 19:39:07 +0530

    Added basic menu system with while loop

---
+++ To_do.py
@@ -0,0 +1,14 @@
+def show_menu():
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Mark Task as Done")
+    print("5. Exit")
+
+
+print("Welcome to To-Do List App")
+
+while True:
+    show_menu()
+    choice = input("Enter your choice: ")
+    
